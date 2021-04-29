c4118dba34271a72316ae7392ddf00b8128bfa07
Charlie Sellers <charliejsellers@gmail.com>
2021-04-29 16:27:55 -0500

    Created placeholders for buy and sell pages

---
+++ pages/buy.tsx
@@ -0,0 +1,7 @@
+export default function Buy(): JSX.Element {
+    return (
+        <h1>
+            This is a placeholder for the BUY page.
+        </h1>
+    );
+};
---
+++ pages/sell.tsx
@@ -0,0 +1,7 @@
+export default function Sell(): JSX.Element {
+    return (
+        <h1>
+            This is a placeholder for the SELL page.
+        </h1>
+    );
+};
