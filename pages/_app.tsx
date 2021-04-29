39b06bc662922e4a667bf0eb79d78721eb22937e
Charlie Sellers <charliejsellers@gmail.com>
2021-04-29 15:58:44 -0500

    Added Navigation component and bootstrap.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
-import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import Navigation from '../components/Navigation';
+import '../styles/globals.css';
+import '../styles/navigation.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -10,8 +12,14 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
           Lit Classifieds
           <link rel="icon" href="/favicon.ico" />
         </title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+          crossOrigin="anonymous"
+        />
       </Head>
-
+      <Navigation />
       <Component {...pageProps} />
     </div>
   )
