631aa2327cc53bfad2bf07199751087163c4af65
Charlie Sellers <charliejsellers@gmail.com>
2021-05-02 17:29:05 -0500

    Phase2 finished

--- components/Phase1.tsx
+++ components/Phase1.tsx
@@ -1,24 +1,74 @@
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import {
-  UserData, PhaseProps,
-} from './interfaces';
+import Image from 'react-bootstrap/Image';
+import Alert from 'react-bootstrap/Alert';
+import { UserData, PhaseProps } from './interfaces';
 import cloneDeep from 'clone-deep';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 
-function Phase1({ userData, userData: { phase1 }, setUserData }: PhaseProps): JSX.Element {
-  const { image, description, price, date } = phase1;
-  console.log(this.imageFile);
+import styles from '../styles/Phase1.module.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function Phase1({ 
+  userData, userData: { phase1 }, setUserData 
+}: PhaseProps): JSX.Element {
+  const [dateMessage, setDateMessage] = useState<boolean>(false);
+  const { image, description, price, date, previewUrl } = phase1;
+
   function setter(stateName: string, newValue: any): void {
     let updated: UserData = cloneDeep(userData);
-    updated.phase0[stateName] = newValue;
+    updated.phase1[stateName] = newValue;
     setUserData(updated);
   };
+
+  // setter() was causing issues here so I made a custom setter
+  function handleFile(file: object): void {
+    let updated: UserData = cloneDeep(userData);
+    updated.phase1.image = file;
+    updated.phase1.previewUrl = URL.createObjectURL(file);
+    setUserData(updated);
+  };
+
+  function validateDate(date: Date): void {
+    if (Date.now() < date.getTime()) {
+      setter('date', date);
+      setDateMessage(false);
+    } else {
+      setDateMessage(true);
+    }
+  };
+
   return (
     <Modal.Body>
       <Form>
-        <Form.Group controlId="imageFile">
-          <Form.File label="Upload an image file" />
+        <Form.Group onChange={(e): void => handleFile(e.target.files[0])}>
+          <Form.File label="Upload an image file" feedbackTooltip />
         </Form.Group>
+        <div className={styles.imageContainer}>
+          <Image
+            src={phase1.previewUrl}
+            rounded
+            className={styles.imagePreview}
+          />
+        </div>
+        <Form.Group onChange={(e): void => setter('description', e.target.value) }>
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+
+        <Form.Label>Pick an end date for this listing</Form.Label>
+        <br />
+        <DatePicker
+          selected={phase1.date}
+          onChange={(date: Date): void => validateDate(date)}
+        />
+
+        {dateMessage &&
+          <Alert variant="danger" className={styles.alert}>
+            Date must not be before the current date. Please try again.
+          </Alert>
+        }
       </Form>
     </Modal.Body>
   );
