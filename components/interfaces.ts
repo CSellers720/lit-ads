7c2d3bbd360df083e8c210d9b60d42d78bd3d69a
Charlie Sellers <charliejsellers@gmail.com>
2021-05-02 09:35:03 -0500

    Created a file for interfaces and enums

---
+++ components/interfaces.ts
@@ -0,0 +1,101 @@
+enum ListingType {
+  Buy = 'BUY',
+  Sell = 'SELL',
+  Null = ''
+}
+
+enum ListingCategory {
+  Electronics = 'ELECTRONICS',
+  Automotive = 'AUTO',
+  Clothing = 'CLOTHING',
+  Furniture = 'FURNITURE',
+  Audio = 'AUDIO',
+  Optical = 'OPTICAL',
+  Misc = 'MISC',
+  Null = '',
+}
+
+enum Tag {
+  Used = 'USED',
+  New = 'NEW',
+  Broken = 'BROKEN',
+  Scrap = 'SCRAP',
+  Parts = 'PARTS',
+  Computer = 'PC',
+  Sport = 'SPORT',
+  Car = 'CAR',
+  Collectible = 'COLLECTIBLE',
+  Instrument = 'INSTRUMENT',
+}
+
+interface ModalProps {
+  showModal: boolean;
+  modalPhase: number;
+  userData: UserData;
+  setUserData: (value: UserData) => void;
+  setShowModal: (value: boolean) => void;
+  setModalPhase: (value: number) => void;
+}
+
+interface ModalHeaderProps {
+  modalPhase: number;
+}
+
+interface ModalBodyProps {
+  modalPhase: number;
+  userData: UserData;
+  setUserData: (value: UserData) => void;
+};
+
+interface ModalFooterProps {
+  modalPhase: number;
+  incrementPhase: () => void;
+  decrementPhase: () => void;
+  toggle: () => void;
+}
+
+interface PhaseProps {
+  userData: UserData;
+  setUserData: (value: UserData) => void;
+}
+
+interface Phase0 {
+  title: string;
+  listing: ListingType;
+  category: ListingCategory;
+}
+
+interface Check {
+  label: string;
+  checked: boolean;
+  value: ListingType | ListingCategory;
+}
+
+interface Phase1 {
+  image: File | null;
+  description: string;
+  price: number;
+  date: Date;
+}
+
+interface Phase2 {
+  condition: number;
+  tags: Array<Tag> | Array<null>;
+  email: string;
+}
+
+interface UserData {
+  phase0: Phase0;
+  phase1: Phase1;
+  phase2: Phase2;
+}
+
+export default ModalProps;
+export type {
+  ModalHeaderProps, ModalFooterProps, UserData,
+  ModalBodyProps, Phase0, Phase1, Phase2, Check,
+  PhaseProps,
+}
+export {
+  ListingType, ListingCategory, Tag,
+}
