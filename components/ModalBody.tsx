1e3ab8b08eef4ae742a35504c6bce4d54eba6edc
Charlie Sellers <charliejsellers@gmail.com>
2021-05-02 13:20:54 -0500

    Cleaned up after extracting phases

--- components/ModalBody.tsx
+++ components/ModalBody.tsx
@@ -1,32 +1,12 @@
 import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
 import Phase0 from './Phase0';
-import {
-  ModalBodyProps, ListingType, ListingCategory, UserData,
-  Radio,
-} from './interfaces';
-import cloneDeep from 'clone-deep';
+import Phase1 from './Phase1';
+import Phase2 from './Phase2';
+import { ModalBodyProps } from './interfaces';
 
 function ModalBody({
-  modalPhase, userData: { phase0, phase1, phase2 }, userData, setUserData,
+  modalPhase, userData, setUserData,
 }: ModalBodyProps): JSX.Element {
-  const radios: Array<Radio> = [
-    { 
-      label: 'Looking to Buy', 
-      checked: phase0.listing === ListingType.Buy,
-      listingType: ListingType.Buy,
-    },
-    {
-      label: 'Looking to Sell',
-      checked: phase0.listing === ListingType.Sell,
-      listingType: ListingType.Sell,
-    },
-  ];
-  function setType(listingType: ListingType): void {
-    let updated: UserData = cloneDeep(userData);
-    updated.phase0.listing = listingType;
-    setUserData(updated);
-  };
   switch(modalPhase) {
     case 0: {
       return (
@@ -38,16 +18,18 @@ function ModalBody({
     }
     case 1: {
       return (
-        <Modal.Body>
-          
-        </Modal.Body>
+        <Phase1
+          userData={userData}
+          setUserData={setUserData}
+        />
       );
     }
     case 2: {
       return (
-        <Modal.Body>
-          
-        </Modal.Body>
+        <Phase2
+          userData={userData}
+          setUserData={setUserData}
+        />
       );
     }
     case 3: {
