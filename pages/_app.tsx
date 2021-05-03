06d2560c4024aed33d081379492d8dd7208a0d15
Charlie Sellers <charliejsellers@gmail.com>
2021-05-02 23:07:11 -0500

    Extracted defaultUserData from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,32 +3,15 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import Navigation from '../components/Navigation';
 import ListingModal from '../components/ListingModal';
-import { UserData, ListingCategory, ListingType } from '../components/interfaces';
+import { UserData, defaultUserData } from '../components/interfaces';
 import '../styles/globals.css';
 import '../styles/navigation.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const [modalPhase, setModalPhase] = useState<number>(0);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [userData, setUserData] = useState<UserData>({
-    phase0: {
-      title: '',
-      listing: ListingType.Null,
-      category: ListingCategory.Null,
-    },
-    phase1: {
-      image: null,
-      previewUrl: '',
-      description: '',
-      date: null,
-    },
-    phase2: {
-      price: 0,
-      condition: 4,
-      tags: [null],
-      email: '',
-    },
-  });
+  const [userData, setUserData] = useState<UserData>(defaultUserData);
+
   return (
     <div>
       <Head>
